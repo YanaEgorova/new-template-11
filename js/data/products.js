56b5e4687f73463a170be6394156ccd5b2b26169
YanaEgorova <yanaegorova69.3@gmail.com>
2021-08-24 02:17:25 +0300

    +

--- js/data/products.js
+++ js/data/products.js
@@ -2,7 +2,7 @@ export const products = [
     {
         id: 'product-1',
         name: 'Rope Ladder Bird Toy',
-        price: 5.95,
+        price: 6.96,
         image: './img/img1.png',
         description: [
             {
@@ -22,29 +22,8 @@ export const products = [
     },
     {
         id: 'product-2',
-        name: 'Oven Mitt',
-        price: 19.99,
-        image: './img/img2.png',
-        description: [
-            {
-                text: '100% Cotton.',
-            },
-            {
-                text: 'Heat resistant oven mitt.',
-            },
-            {
-                text: 'Unique opposable thumb construction for a tight grip.',
-            },
-            {
-                text: 'Right handed design.',
-            },
-        ],
-        images: ['./img/img2-1.png', './img/img2-2.png'],
-    },
-    {
-        id: 'product-3',
         name: 'Black Mug',
-        price: 24.99,
+        price: 29.99,
         discount: 14.99,
         image: './img/img3.png',
         description: [
@@ -67,128 +46,9 @@ export const products = [
         images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png'],
     },
     {
-        id: 'product-4',
-        name: 'Cutting Board',
-        price: 35.32,
-        discount: 24.65,
-        image: './img/img4.png',
-        description: [
-            {
-                text: 'Manufactured in the country of China.',
-            },
-            {
-                text: 'Items Color: Natural.',
-            },
-            {
-                text: 'Decoration: engraved.',
-            },
-            {
-                text: 'Handsome cutting board in a football design with lazer engraved team logo.',
-            },
-            {
-                text: 'Bamboo is a renewable, sustainable resource.',
-            },
-            {
-                text: 'Dark bamboo design with light bamboo inlays.',
-            },
-            {
-                text: 'Great for game day.',
-            },
-            {
-                text: 'Measures 15 by 8-3/4 inches.',
-            },
-        ],
-        images: ['./img/img4-1.png', './img/img4-2.png'],
-    },
-    {
-        id: 'product-5',
-        name: 'Elastic Band Fishing Headwear',
-        price: 39.99,
-        discount: 39.97,
-        image: './img/img5.png',
-        description: [
-            {
-                title: 'Unique design',
-                text: 'This fun umbrella hat use very little space for portable travel. Stylish and functional accessories, practical design keeps you dry while keeping your hands free. If you don\'t know whether the hat umbrella is suitable for you, please buy it according to the size we provide.',
-            },
-            {
-                title: 'Function',
-                text: 'Oxford cloth top bounces light off so don\'t absorbed heat and anti-UV, which keeps you much cooler with the sun off your head. 8 metal ribs makes umbrella very strong, and in rainy weather, it was completely against the wind.',
-            },
-            {
-                title: 'Occasion',
-                text: 'Great for parties or other fun family outings. Wear this umbrella hat in the rain or shine. Suitable for gardening, hiking, farming, golfing and Halloween costume etc. This hat is a great personal sunshade at the beach or for fishing Too.',
-            },
-            {
-                title: 'Fit most',
-                text: 'This hat features an adjustable strap, small enough to fit a child\'s head but stretches big enough to fit a large adult head. Adjustable stape make umbrella hat tightly in place on your head. Which keep the colorful hat on your head comfortably even in the wind.',
-            },
-        ],
-        images: ['./img/img5-1.jpg', './img/img5-2.jpg', './img/img5-3.png', './img/img5-4.jpg', './img/img5-5.png'],
-    },
-    {
-        id: 'product-6',
-        name: 'Moisture Meter',
-        price: 43.98,
-        discount: 55.97,
-        image: './img/img6.png',
-        description: [
-            {
-                title: 'Measures',
-                text: 'Separate settings for drywall, masonry, softwood and hardwood.',
-            },
-            {
-                title: 'No drywall damage',
-                text: 'Non-marring sensing technology protects drywall.',
-            },
-            {
-                title: 'Easy to read',
-                text: 'LEDs clearly indicate moisture level.',
-            },
-            {
-                title: 'HI visibility screen',
-                text: 'Large 2 inch (50mm) backlit white on black screen providing easier readability from any angle.',
-            },
-            {
-                title: 'Find mold',
-                text: 'Stops mold before it starts with early detection of moisture; essential water damage tool.',
-            },
-            {
-                title: 'Ergonomic',
-                text: 'Improved ergonomic handle design conforms to your hand for a more comfortable user experience.',
-            },
-        ],
-        images: ['./img/img6-1.png', './img/img6-2.png', './img/img6-3.png', './img/img6-4.png'],
-    },
-    {
-        id: 'product-7',
-        name: 'Yarn Bowl',
-        price: 49.95,
-        discount: 69.99,
-        image: './img/img7.png',
-        description: [
-            {
-                text: 'Is it a project bag? A yarn bowl? It\'s both!',
-            },
-            {
-                text: 'Closed, your yarn and hook or needles are protected, secure and tidy, ready to be tucked away in a bag, a briefcase, or conveniently looped over your wrist.',
-            },
-            {
-                text: 'Open, it becomes a sturdy bowl to keep your yarns from twisting or rolling away as you work.',
-            },
-            {
-                text: 'Made of ultra-lightweight, flexible silicon, there is nothing to snag or damage your project.',
-            },
-            {
-                text: 'Available in a choice of three jewel shades, with a whimsical design of knitting stitches, this innovative project caddy takes your knit and crochet experience to the next level.',
-            },
-        ],
-        images: ['./img/img7-1.png', './img/img7-2.png'],
-    },
-    {
-        id: 'product-8',
+        id: 'product-3',
         name: 'Convertible Hand Truck',
-        price: 69.99,
+        price: 64.90,
         discount: 69.94,
         image: './img/img8.png',
         description: [
@@ -211,65 +71,9 @@ export const products = [
         images: ['./img/img8-1.png', './img/img8-2.png'],
     },
     {
-        id: 'product-9',
-        name: 'Steel Rollator Walker',
-        price: 79.99,
-        discount: 79.93,
-        image: './img/img9.png',
-        description: [
-            {
-                text: 'Steel Rollator walker supports up to 350 lbs.',
-            },
-            {
-                text: '6 inch wheels. Seat dimensions: 12 x 12 inches.',
-            },
-            {
-                text: 'Comfortable padded seat and backrest allow users to sit if they become tired.',
-            },
-            {
-                text: 'Bag under seat provides additional storage.',
-            },
-            {
-                text: 'Rolling walker weighs 19 lbs and folds flat for easy storage and transportation.',
-            },
-        ],
-        images: ['./img/img9-1.png', './img/img9-2.png', './img/img9-3.png'],
-    },
-    {
-        id: 'product-10',
-        name: 'Dry Food Dispenser',
-        price: 84.41,
-        discount: 95.50,
-        image: './img/img10.png',
-        description: [
-            {
-                text: 'Heavy-duty for commercial use but also suitable for the everyday kitchen.',
-            },
-            {
-                text: 'Designed to preserve freshness up to 45 days.',
-            },
-            {
-                text: 'Each canister holds up to 17.5 ounces of cereal or dried goods.',
-            },
-            {
-                text: 'Dimensions: 6.5"L x 7"W x 15"H',
-            },
-            {
-                text: 'Dry-food dispenser with 17-1/2-ounce scratch-resistant canister.',
-            },
-            {
-                text: 'Can hold a full box of cereal; preserves freshness for up to 45 days.',
-            },
-            {
-                text: 'Easy-turn dispensing knob; portion-control system; won\'t crush cereal.',
-            },
-        ],
-        images: ['./img/img10-1.png', './img/img10-2.png', './img/img10-3.png'],
-    },
-    {
-        id: 'product-11',
+        id: 'product-4',
         name: 'Electric Pencil Sharpener',
-        price: 89.99,
+        price: 98.90,
         discount: 109.91,
         image: './img/img11.png',
         description: [
@@ -292,9 +96,9 @@ export const products = [
         images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-3.png'],
     },
     {
-        id: 'product-12',
+        id: 'product-5',
         name: 'Trike',
-        price: 109.00,
+        price: 119.98,
         discount: 110.89,
         image: './img/img12.png',
         description: [
@@ -328,9 +132,9 @@ export const products = [
         images: ['./img/img12-1.png', './img/img12-2.png', './img/img12-3.jpg'],
     },
     {
-        id: 'product-13',
+        id: 'product-6',
         name: 'Mattress 4 Inch Trifold',
-        price: 119.00,
+        price: 148.00,
         discount: 120.88,
         image: './img/img13.png',
         description: [
