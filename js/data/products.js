9625e7902f7becb2c0381c7ca48e3745870fb2ab
YanaEgorova <yanaegorova69.3@gmail.com>
2021-08-23 19:39:04 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,355 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Rope Ladder Bird Toy',
+        price: 5.95,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Naturals toys are handmade from 100-percent natural, sustainable, materials.',
+            },
+            {
+                text: 'Your bird will benefit from hours of mental stimulation and rugged physical play.',
+            },
+            {
+                text: 'A variety of natural textures encourages foot exercise.',
+            },
+            {
+                text: 'Connects to cage easily with quick-link attachment.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Oven Mitt',
+        price: 19.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: '100% Cotton.',
+            },
+            {
+                text: 'Heat resistant oven mitt.',
+            },
+            {
+                text: 'Unique opposable thumb construction for a tight grip.',
+            },
+            {
+                text: 'Right handed design.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Black Mug',
+        price: 24.99,
+        discount: 14.99,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'The 10 oz Stackable Mug is made from durable stainless steel with double-wall vacuum insulation to protect your hot or cold beverage at all costs.',
+            },
+            {
+                text: 'The only drink lid that uses the power of magnets to keep your favorite drink on lock.',
+            },
+            {
+                text: 'While the Lid adds a barrier of protection for keeping drinks contained and preventing heat or cold from escaping, Please Note – The Lid is not leakproof and will not prevent spills.',
+            },
+            {
+                text: 'There are BPA-free, dishwasher safe, and have a No Sweat Design to make sure your hands stay dry. The tough Duracoat coating on the colored tumblers won’t crack, peel or fade.',
+            },
+            {
+                text: 'The 10 oz. Mug stands 4 inches high and has a lip diameter of 3.5 inches, and with the handle its almost 5 inches’ wide.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Cutting Board',
+        price: 35.32,
+        discount: 24.65,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Manufactured in the country of China.',
+            },
+            {
+                text: 'Items Color: Natural.',
+            },
+            {
+                text: 'Decoration: engraved.',
+            },
+            {
+                text: 'Handsome cutting board in a football design with lazer engraved team logo.',
+            },
+            {
+                text: 'Bamboo is a renewable, sustainable resource.',
+            },
+            {
+                text: 'Dark bamboo design with light bamboo inlays.',
+            },
+            {
+                text: 'Great for game day.',
+            },
+            {
+                text: 'Measures 15 by 8-3/4 inches.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Elastic Band Fishing Headwear',
+        price: 39.99,
+        discount: 39.97,
+        image: './img/img5.png',
+        description: [
+            {
+                title: 'Unique design',
+                text: 'This fun umbrella hat use very little space for portable travel. Stylish and functional accessories, practical design keeps you dry while keeping your hands free. If you don\'t know whether the hat umbrella is suitable for you, please buy it according to the size we provide.',
+            },
+            {
+                title: 'Function',
+                text: 'Oxford cloth top bounces light off so don\'t absorbed heat and anti-UV, which keeps you much cooler with the sun off your head. 8 metal ribs makes umbrella very strong, and in rainy weather, it was completely against the wind.',
+            },
+            {
+                title: 'Occasion',
+                text: 'Great for parties or other fun family outings. Wear this umbrella hat in the rain or shine. Suitable for gardening, hiking, farming, golfing and Halloween costume etc. This hat is a great personal sunshade at the beach or for fishing Too.',
+            },
+            {
+                title: 'Fit most',
+                text: 'This hat features an adjustable strap, small enough to fit a child\'s head but stretches big enough to fit a large adult head. Adjustable stape make umbrella hat tightly in place on your head. Which keep the colorful hat on your head comfortably even in the wind.',
+            },
+        ],
+        images: ['./img/img5-1.jpg', './img/img5-2.jpg', './img/img5-3.png', './img/img5-4.jpg', './img/img5-5.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Moisture Meter',
+        price: 43.98,
+        discount: 55.97,
+        image: './img/img6.png',
+        description: [
+            {
+                title: 'Measures',
+                text: 'Separate settings for drywall, masonry, softwood and hardwood.',
+            },
+            {
+                title: 'No drywall damage',
+                text: 'Non-marring sensing technology protects drywall.',
+            },
+            {
+                title: 'Easy to read',
+                text: 'LEDs clearly indicate moisture level.',
+            },
+            {
+                title: 'HI visibility screen',
+                text: 'Large 2 inch (50mm) backlit white on black screen providing easier readability from any angle.',
+            },
+            {
+                title: 'Find mold',
+                text: 'Stops mold before it starts with early detection of moisture; essential water damage tool.',
+            },
+            {
+                title: 'Ergonomic',
+                text: 'Improved ergonomic handle design conforms to your hand for a more comfortable user experience.',
+            },
+        ],
+        images: ['./img/img6-1.png', './img/img6-2.png', './img/img6-3.png', './img/img6-4.png'],
+    },
+    {
+        id: 'product-7',
+        name: 'Yarn Bowl',
+        price: 49.95,
+        discount: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                text: 'Is it a project bag? A yarn bowl? It\'s both!',
+            },
+            {
+                text: 'Closed, your yarn and hook or needles are protected, secure and tidy, ready to be tucked away in a bag, a briefcase, or conveniently looped over your wrist.',
+            },
+            {
+                text: 'Open, it becomes a sturdy bowl to keep your yarns from twisting or rolling away as you work.',
+            },
+            {
+                text: 'Made of ultra-lightweight, flexible silicon, there is nothing to snag or damage your project.',
+            },
+            {
+                text: 'Available in a choice of three jewel shades, with a whimsical design of knitting stitches, this innovative project caddy takes your knit and crochet experience to the next level.',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png'],
+    },
+    {
+        id: 'product-8',
+        name: 'Convertible Hand Truck',
+        price: 69.99,
+        discount: 69.94,
+        image: './img/img8.png',
+        description: [
+            {
+                text: 'Glass Filled Nylon Plastic Frame is 30% lighter than steel and designed for maximum strength.',
+            },
+            {
+                text: 'Plastic Frame is factory assembled and is chemical, corrosion and rust resistant for longer life.',
+            },
+            {
+                text: 'Converts in seconds from 2 wheel dolly to 4 wheel cart - 300 lb dolly and 400 lb cart capacity (Product images illustrates both uses, but only one cart is being sold).',
+            },
+            {
+                text: '8" wheels with solid rubber tread and 3" swivel casters.',
+            },
+            {
+                text: 'Three position telescoping handle, built-in frame glides for easy moving up or down stairs and curbs.',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png'],
+    },
+    {
+        id: 'product-9',
+        name: 'Steel Rollator Walker',
+        price: 79.99,
+        discount: 79.93,
+        image: './img/img9.png',
+        description: [
+            {
+                text: 'Steel Rollator walker supports up to 350 lbs.',
+            },
+            {
+                text: '6 inch wheels. Seat dimensions: 12 x 12 inches.',
+            },
+            {
+                text: 'Comfortable padded seat and backrest allow users to sit if they become tired.',
+            },
+            {
+                text: 'Bag under seat provides additional storage.',
+            },
+            {
+                text: 'Rolling walker weighs 19 lbs and folds flat for easy storage and transportation.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png', './img/img9-3.png'],
+    },
+    {
+        id: 'product-10',
+        name: 'Dry Food Dispenser',
+        price: 84.41,
+        discount: 95.50,
+        image: './img/img10.png',
+        description: [
+            {
+                text: 'Heavy-duty for commercial use but also suitable for the everyday kitchen.',
+            },
+            {
+                text: 'Designed to preserve freshness up to 45 days.',
+            },
+            {
+                text: 'Each canister holds up to 17.5 ounces of cereal or dried goods.',
+            },
+            {
+                text: 'Dimensions: 6.5"L x 7"W x 15"H',
+            },
+            {
+                text: 'Dry-food dispenser with 17-1/2-ounce scratch-resistant canister.',
+            },
+            {
+                text: 'Can hold a full box of cereal; preserves freshness for up to 45 days.',
+            },
+            {
+                text: 'Easy-turn dispensing knob; portion-control system; won\'t crush cereal.',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.png', './img/img10-3.png'],
+    },
+    {
+        id: 'product-11',
+        name: 'Electric Pencil Sharpener',
+        price: 89.99,
+        discount: 109.91,
+        image: './img/img11.png',
+        description: [
+            {
+                text: 'Commercial grade pencil sharpener for high volume environments.',
+            },
+            {
+                text: 'Dual helical cutters sharpen pencils with precision.',
+            },
+            {
+                text: 'Efficient electric motor provides reliable power; Pencil Saver feature prevents over sharpening.',
+            },
+            {
+                text: 'Electric Pencil Sharpener technology eliminates oversharpening and extends pencil life.',
+            },
+            {
+                text: 'Auto Reset and SafeStart help protect sharpener and user.',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-3.png'],
+    },
+    {
+        id: 'product-12',
+        name: 'Trike',
+        price: 109.00,
+        discount: 110.89,
+        image: './img/img12.png',
+        description: [
+            {
+                title: '4-in-1 multifunctional bike for kids',
+                text: 'A great birthday gift for kids is a bike that grows with your child. The Tikes 4-in-1 comes in 4 stages that cover infant stroll, pushing toddler trike, learning balance, and kids pedaling on their own.',
+            },
+            {
+                title: 'Secure child protection',
+                text: 'In the 1st and 2nd stage, protect your child from direct sunlight and heat with an adjustable canopy. Accessories also includes a removable waist bar, 5-point seatbelt harness, and detachable footrest for a comfortable, safe stroll.',
+            },
+            {
+                title: 'Other safety features',
+                text: 'This infant trike offers balance even without training wheels. Parents can keep the trike in place with rear wheel locks. It also has non-slip pedals to keep your child\'s feet protected.',
+            },
+            {
+                title: 'Grows with kids',
+                text: 'The 4-in-1 Trike grows with young children thanks to the adjustable sliding seat. The push bar has a parent-steering feature while the toddler learns to pedal. It can be removed as kids learn to ride the trike on their own.',
+            },
+            {
+                title: 'Store while strolling',
+                text: 'Adults can bring along whatever they need thanks to the spacious basket/bucket behind the seat. Parents\' push handle has a tray and cup holder',
+            },
+            {
+                text: 'Max rider weight is 50 lbs.',
+            },
+            {
+                text: 'Assembly required.',
+            },
+        ],
+        images: ['./img/img12-1.png', './img/img12-2.png', './img/img12-3.jpg'],
+    },
+    {
+        id: 'product-13',
+        name: 'Mattress 4 Inch Trifold',
+        price: 119.00,
+        discount: 120.88,
+        image: './img/img13.png',
+        description: [
+            {
+                text: 'Memory Foam.',
+            },
+            {
+                text: '1. 5" Memory Foam Top layer plus 2. 5" High-density base foam layer for added comfort and support.',
+            },
+            {
+                text: 'Great for camping, overnight guests, car trips and sleepovers.',
+            },
+            {
+                text: '100% poly-Jacquard cover with slip-proof Bottom.',
+            },
+            {
+                text: 'Certipur-us Certified memory and high-density foam.',
+            },
+        ],
+        images: ['./img/img13-1.jpg', './img/img13-2.jpg', './img/img13-3.jpg', './img/img13-4.jpg', './img/img13-5.jpg'],
+    },
+]
